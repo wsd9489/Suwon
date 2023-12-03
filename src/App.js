5bbc0011090ca25dcd2b1d29d25997ea16851682
pjookim <pjookim@naver.com>
2023-12-03 20:03:29 +0900

    messages sequence added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import youngjo from './assets/images/young.png'
+import youngjo from './assets/images/youngjo.png'
+import youngjo_angry from './assets/images/youngjo_angry.png'
 import jeongjo from './assets/images/jeongjo.png'
+import jeongjo_shine from './assets/images/jeongjo_shine.png'
+import jeongjo_cry from './assets/images/jeongjo_cry.png'
 import sadoseja from './assets/images/sadoseja.png'
+import sadoseja_sad from './assets/images/sadoseja_sad.png'
 import './App.css';
 import BubbleChat from './components/speech_bubble';
 
 function App() {
+  const [step, setStep] = useState(1);
+
+  const handleNext = () => {
+    setStep(step + 1);
+  };
+
+  const handleFinish = () => {
+    alert('끝끝~');
+    // 여기에서 필요한 완료 처리를 수행할 수 있습니다.
+  };
   const messages1 = [
     '안녕하세요! 저는 영조예요.',
     '어떤 내용을 표시할까요?',
@@ -29,7 +44,9 @@ function App() {
   ];
   return (
     <div className="flex bg-[#8581A6] h-screen w-screen">
-        <BubbleChat messages={messages1} img={youngjo}/>
+        {step === 1 && <BubbleChat messages={messages1} img={youngjo} onNext={handleNext} />}
+        {step === 2 && <BubbleChat messages={messages2} img={sadoseja} onNext={handleNext} />}
+        {step === 3 && <BubbleChat messages={messages3} img={jeongjo} onNext={handleFinish} />}
     </div>
   );
 }
--- src/components/speech_bubble.js
+++ src/components/speech_bubble.js
@@ -10,26 +10,28 @@ const BubbleChat = (props) => {
     if (currentMessageIndex < props.messages.length - 1) {
       setCurrentMessageIndex(currentMessageIndex + 1);
       setKey((prevKey) => prevKey + 1);
+    } else {
+      props.onNext();
     }
   };
 
   return (
-    <div className="flex justify-center items-end w-full relative">
-        <img src={props.img} className="w-72 absolute -right-2 bottom-0" alt="logo" />
+    <div className="flex justify-center items-end w-full relative overflow-hidden">
+        <img src={props.img} className="w-72 absolute -right-2 -bottom-8" alt="logo" />
       <div className="bg-white shadow-md ring-1 ring-inset ring-gray-300 text-grey p-8 rounded-2xl mb-16 w-4/5 max-w-5xl">
         <TypeAnimation key={key}
         sequence={[
             props.messages[currentMessageIndex],
             1000
         ]} className='text-2xl' wrapper='p'>{props.messages[currentMessageIndex]}</TypeAnimation>
-        {currentMessageIndex < props.messages.length - 1 && (
+        {
           <button
             className="bg-purple-300 text-white border-none px-4 py-2 mt-4 cursor-pointer rounded-lg"
             onClick={handleNextClick}
           >
             다음
           </button>
-        )}
+        }
       </div>
     </div>
   );
